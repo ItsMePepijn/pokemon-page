824f309425561e7a3c29a18c452f508129eb155b
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-03-22 11:01:26 +0100

    removed logging

--- src/modules/getPokemon.js
+++ src/modules/getPokemon.js
@@ -1,6 +1,5 @@
 export default async function getPokemon(offset){
   try {
-    console.log(offset)
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=30&offset=${offset}`)
     .then(res => res.json())
     return res.results;
